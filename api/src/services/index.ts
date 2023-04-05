5b816b46d14f89c2e10badbf161e033f60b8bf1f
fredmessias43 <fredmessias43@gmail.com>
2023-04-05 01:09:06 -0300

    start adding feather.js

---
+++ api/src/services/index.ts
@@ -0,0 +1,8 @@
+import { message } from './messages/messages'
+// For more information about this file see https://dove.feathersjs.com/guides/cli/application.html#configure-functions
+import type { Application } from '../declarations'
+
+export const services = (app: Application) => {
+  app.configure(message)
+  // All services will be registered here
+}
